5ad051c3bcb676c9504ebd4599d9400f9c9bcbdb
Digg¢ <70993871+digger1213@users.noreply.github.com>
2024-07-17 00:58:19 +1200

    Rework config

--- source/Plugin.cs
+++ source/Plugin.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using BepInEx.Configuration;
 using BepInEx.Logging;
 
 namespace CruiserImproved; 
@@ -24,56 +23,5 @@ internal class CruiserImproved : BaseUnityPlugin
         harmony = new Harmony(modGUID);
         UserConfig.InitConfig();
         harmony.PatchAll();
-    }
-
-    public void BindConfig<T>(ref ConfigEntry<T> config, string section, string key, T defaultValue, string description = "")
-    {
-        config = Config.Bind(section, key, defaultValue, description);
-    }
-}
-
-internal class UserConfig
-{
-    //General
-    internal static ConfigEntry<bool> AllowLean;
-    internal static ConfigEntry<bool> PreventMissileKnockback;
-    internal static ConfigEntry<bool> AllowPushDestroyedCar;
-    internal static ConfigEntry<bool> PreventPassengersEjectingDriver;
-    internal static ConfigEntry<bool> EntitiesAvoidCruiser;
-    internal static ConfigEntry<bool> SilentCollisions;
-    internal static ConfigEntry<float> SeatBoostScale;
-
-    //Cruiser Health
-    internal static ConfigEntry<float> CruiserInvulnerabilityDuration;
-    internal static ConfigEntry<float> CruiserCriticalInvulnerabilityDuration;
-    internal static ConfigEntry<int> MaxCriticalHitCount;
-
-    //Physics
-    internal static ConfigEntry<bool> AntiSideslip;
-    internal static void InitConfig()
-    {
-        ConfigFile config = CruiserImproved.Instance.Config;
-
-        AllowLean = config.Bind("General", "Allow Leaning", true, "If true, allow the player to look backward out the window or through the cabin window.");
-        PreventMissileKnockback = config.Bind("General", "Prevent Missile Knockback", true, "If true, prevent the player being ejected from seats by Old Bird missile knockback.");
-        AllowPushDestroyedCar = config.Bind("General", "Allow Pushing Destroyed Cruisers", true, "If true, allow players to push destroyed cruisers.");
-        EntitiesAvoidCruiser = config.Bind("General", "Entities Avoid Cruiser", true, "If true, entities will pathfind around stationary cruisers with no driver.\nEyeless Dogs will still attack it if they hear noise!");
-        SilentCollisions = config.Bind("General", "Silent Collisions", true, "If true, entities hitting the Cruiser when it's engine is off will not make noise.\nThis means Eyeless Dogs will not get stuck in a loop attacking it, triggering noise, and attacking it again while the engine is off.");
-        PreventPassengersEjectingDriver = config.Bind("General", "Prevent Passengers Eject Driver", false, "If true, prevent anyone except the driver of the cruiser from using the eject button in lobbies you host.");
-
-        AcceptableValueRange<float> seatScale = new(0f, 1f);
-        SeatBoostScale = config.Bind("General", "Seat Boost Scale", 1.0f, new ConfigDescription("How much to boost the seat up? Set 0 to disable.", seatScale));
-
-        AcceptableValueRange<float> invulnerableDuration = new(0f, 2f);
-        CruiserInvulnerabilityDuration = config.Bind("Cruiser Health", "Cruiser Invulnerability Duration", 0.5f, new ConfigDescription("How long after taking damage is the Cruiser invulnerable for? Set 0 to disable.", invulnerableDuration));
-
-        AcceptableValueRange<float> criticalInvulnerableDuration = new(0f, 6f);
-        CruiserCriticalInvulnerabilityDuration = config.Bind("Cruiser Health", "Cruiser Critical Invulnerability Duration", 4.0f, new ConfigDescription("How long after critical damage (engine on fire) is the Cruiser invulnerable for? Set 0 to disable.", criticalInvulnerableDuration));
-
-
-        AcceptableValueRange<int> criticalHitCount = new(0, 100);
-        MaxCriticalHitCount = config.Bind("Cruiser Health", "Critical Protection Hit Count", 1, new ConfigDescription("Number of hits the Cruiser can block during the Critical Invulnerability Duration. \nIf the Cruiser receives this many hits while critical, it will emit a sound cue before exploding once the duration is up.\nIf 0, any hit that triggers the critical state will also trigger this delayed explosion.", criticalHitCount));
-
-        AntiSideslip = config.Bind("Physics", "Anti-Sideslip", true, "If true, prevent the Cruiser from sliding sideways when on slopes.");
     }
 }
---
+++ source/UserConfig.cs
@@ -0,0 +1,91 @@
+﻿using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CruiserImproved;
+
+internal class UserConfig
+{
+    //General
+    internal static ConfigEntry<bool> AllowLean;
+    internal static ConfigEntry<bool> PreventMissileKnockback;
+    internal static ConfigEntry<bool> AllowPushDestroyedCar;
+    internal static ConfigEntry<bool> PreventPassengersEjectingDriver;
+    internal static ConfigEntry<bool> EntitiesAvoidCruiser;
+    internal static ConfigEntry<bool> SilentCollisions;
+    internal static ConfigEntry<float> SeatBoostScale;
+
+    //Cruiser Health
+    internal static ConfigEntry<float> CruiserInvulnerabilityDuration;
+    internal static ConfigEntry<float> CruiserCriticalInvulnerabilityDuration;
+    internal static ConfigEntry<int> MaxCriticalHitCount;
+
+    //Physics
+    internal static ConfigEntry<bool> AntiSideslip;
+
+    //Hosting
+    internal static ConfigEntry<bool> SyncSeatSettings;
+
+    internal static Dictionary<ConfigDefinition, ConfigEntryBase> ConfigMigrations;
+
+    internal static void InitConfig()
+    {
+        ConfigFile config = CruiserImproved.Instance.Config;
+
+        config.SaveOnConfigSet = false;
+
+        //General
+        AllowLean = config.Bind("General", "Allow Leaning", true, "If true, allow the player to look backward out the window or through the cabin window.");
+        PreventMissileKnockback = config.Bind("General", "Prevent Missile Knockback", true, "If true, prevent the player being ejected from seats by Old Bird missile knockback.");
+        AllowPushDestroyedCar = config.Bind("General", "Allow Pushing Destroyed Cruisers", true, "If true, allow players to push destroyed cruisers.");
+        SilentCollisions = config.Bind("General", "Silent Collisions", true, "If true, entities hitting the Cruiser when it's engine is off will not make noise.\nThis means Eyeless Dogs will not get stuck in a loop attacking it, triggering noise, and attacking it again while the engine is off.");
+
+        AcceptableValueRange<float> seatScale = new(0f, 1f);
+        SeatBoostScale = config.Bind("General", "Seat Boost Scale", 1.0f, new ConfigDescription("How much to boost the seat up? Set 0 to disable.", seatScale));
+
+        //Cruiser Health
+        AcceptableValueRange<float> invulnerableDuration = new(0f, 2f);
+        CruiserInvulnerabilityDuration = config.Bind("Cruiser Health", "Cruiser Invulnerability Duration", 0.5f, new ConfigDescription("How long after taking damage is the Cruiser invulnerable for? Set 0 to disable.", invulnerableDuration));
+
+        AcceptableValueRange<float> criticalInvulnerableDuration = new(0f, 6f);
+        CruiserCriticalInvulnerabilityDuration = config.Bind("Cruiser Health", "Cruiser Critical Invulnerability Duration", 4.0f, new ConfigDescription("How long after critical damage (engine on fire) is the Cruiser invulnerable for? Set 0 to disable.", criticalInvulnerableDuration));
+
+
+        AcceptableValueRange<int> criticalHitCount = new(0, 100);
+        MaxCriticalHitCount = config.Bind("Cruiser Health", "Critical Protection Hit Count", 1, new ConfigDescription("Number of hits the Cruiser can block during the Critical Invulnerability Duration. \nIf the Cruiser receives this many hits while critical, it will emit a sound cue before exploding once the duration is up.\nIf 0, any hit that triggers the critical state will also trigger this delayed explosion.", criticalHitCount));
+
+        //Physics
+        AntiSideslip = config.Bind("Physics", "Anti-Sideslip", true, "If true, prevent the Cruiser from sliding sideways when on slopes.");
+
+        //Host-side
+        //SyncSeatSettings = config.Bind("Host-side", "Synchronise Seat", false, "If true, set all other players using CruiserImproved in your lobbies to have the same Seat Boost Scale and Allow Leaning setting as you.");
+        EntitiesAvoidCruiser = config.Bind("Host-side", "Entities Avoid Cruiser", true, "If true, entities will pathfind around stationary cruisers with no driver.\nEyeless Dogs will still attack it if they hear noise!");
+        PreventPassengersEjectingDriver = config.Bind("Host-side", "Prevent Passengers Eject Driver", false, "If true, prevent anyone except the driver of the cruiser from using the eject button in your lobbies.");
+        MigrateOldConfigs(config);
+        config.Save();
+        config.SaveOnConfigSet = true;
+    }
+
+    static void MigrateOldConfigs(ConfigFile config)
+    {
+        ConfigMigrations = new()
+        {
+            { new("General", "Prevent Passengers Eject Driver"), PreventPassengersEjectingDriver },
+            { new("General", "Entities Avoid Cruiser"), EntitiesAvoidCruiser },
+        };
+
+        PropertyInfo orphanedEntriesProperty = AccessTools.Property(typeof(ConfigFile), "OrphanedEntries");
+        var orphanedEntries = (Dictionary<ConfigDefinition, string>)orphanedEntriesProperty.GetValue(config);
+        foreach(var entry in orphanedEntries )
+        {
+            if(ConfigMigrations.TryGetValue(entry.Key, out var newConfig))
+            {
+                CruiserImproved.Log.LogMessage("Migrated old config from " + entry.Key + " with value " + entry.Value + " to " + newConfig);
+                newConfig.SetSerializedValue(entry.Value);
+            }
+        }
+        CruiserImproved.Log.LogMessage("Finished migration");
+        orphanedEntries.Clear();
+    }
+}
