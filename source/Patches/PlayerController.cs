f5c4cdfa97b223c7837318928b978bb610114609
wongnata <96847919+wongnata@users.noreply.github.com>
2024-09-29 23:32:43 -0600

    Updated PlayerControllerB patch to only update camera position when in a vehicle.

--- source/Patches/PlayerController.cs
+++ source/Patches/PlayerController.cs
@@ -1,12 +1,4 @@
 ﻿using CruiserImproved.Network;
-using CruiserImproved.Utils;
-using GameNetcodeStuff;
-using HarmonyLib;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Reflection;
-using UnityEngine;
 
 namespace CruiserImproved.Patches;
 
@@ -22,22 +14,22 @@ internal class PlayerControllerPatches
         bool cameraSettingsEnabled = NetworkSync.Config.AllowLean || NetworkSync.Config.SeatBoostScale > 0f;
         if (!cameraSettingsEnabled) return;
 
-        Vector3 cameraOffset = Vector3.zero;
         if (__instance.inVehicleAnimation)
         {
+            Vector3 cameraOffset = Vector3.zero;
             //If we're in a car, boost the camera upward slightly for better visibility
             cameraOffset = new Vector3(0f, 0.25f, -0.05f) * NetworkSync.Config.SeatBoostScale;
             Vector3 lookFlat = __instance.gameplayCamera.transform.localRotation * Vector3.forward;
             lookFlat.y = 0;
             float angleToBack = Vector3.Angle(lookFlat, Vector3.back);
-            if(angleToBack < 70 && NetworkSync.Config.AllowLean)
+            if (angleToBack < 70 && NetworkSync.Config.AllowLean)
             {
                 //If we're looking backwards, offset the camera to the side ('leaning')
-                cameraOffset.x = Mathf.Sign(lookFlat.x) * ((70f - angleToBack)/70f);
+                cameraOffset.x = Mathf.Sign(lookFlat.x) * ((70f - angleToBack) / 70f);
             }
-        }
 
-        __instance.gameplayCamera.transform.localPosition = cameraOffset;
+            __instance.gameplayCamera.transform.localPosition = cameraOffset;
+        }
     }
 
     [HarmonyPatch("PlaceGrabbableObject")]
