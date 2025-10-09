397784ff2585b6eae71ece961d1e1a3969299f04
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2025-10-09 15:40:08 +0100

    Update VehicleController.cs

--- source/Patches/VehicleController.cs
+++ source/Patches/VehicleController.cs
@@ -1026,7 +1026,9 @@ internal class VehicleControllerPatches
         // Sync the tyre skidding effects 
         if (__instance.IsOwner)
         {
-            if (((Time.realtimeSinceStartup - vehicleData[__instance].timeSinceTyreSkidSync) > 0.1f && __instance.skiddingAudio.volume != vehicleData[__instance].lastTyreStress) || (__instance.skiddingAudio.isPlaying != vehicleData[__instance].lastTyreStressPlaying))
+            if (((Time.realtimeSinceStartup - vehicleData[__instance].timeSinceTyreSkidSync) > 0.1f && 
+				 __instance.skiddingAudio.volume != vehicleData[__instance].lastTyreStress) || 
+				(__instance.skiddingAudio.isPlaying != vehicleData[__instance].lastTyreStressPlaying))
             {
 		        vehicleData[__instance].timeSinceTyreSkidSync = Time.realtimeSinceStartup;    
                 FastBufferWriter bufferWriter = new(16, Unity.Collections.Allocator.Temp);
@@ -1041,7 +1043,9 @@ internal class VehicleControllerPatches
 
         // Play the skidding effects on clients sides
         float stressAmount = vehicleData[__instance].lastTyreStress;
-        bool tyreStressing = vehicleData[__instance].lastTyreStressPlaying && stressAmount > 0.3f && __instance.gear == CarGearShift.Drive && __instance.ignitionStarted;
+        bool tyreStressing = vehicleData[__instance].lastTyreStressPlaying && 
+			stressAmount > 0.3f && __instance.gear == CarGearShift.Drive && 
+			__instance.ignitionStarted;
         bool tyreSparksActive = (tyreStressing && __instance.averageVelocity.magnitude > 8f);
         __instance.SetVehicleAudioProperties(__instance.skiddingAudio, tyreStressing, 0f, stressAmount, 3f, true, 1f);
 
