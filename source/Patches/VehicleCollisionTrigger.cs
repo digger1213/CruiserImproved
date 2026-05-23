e050500a88dbf2626518e02ea8cd7b9a353a8874
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2026-05-23 01:18:14 +0100

    Update VehicleCollisionTrigger.cs

--- source/Patches/VehicleCollisionTrigger.cs
+++ source/Patches/VehicleCollisionTrigger.cs
@@ -1,6 +1,10 @@
-﻿using CruiserImproved.Network;
+using CruiserImproved.Network;
 using GameNetcodeStuff;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace CruiserImproved.Patches;
@@ -8,6 +12,36 @@ namespace CruiserImproved.Patches;
 [HarmonyPatch(typeof(VehicleCollisionTrigger))]
 internal class VehicleCollisionTriggerPatches
 {
+    [HarmonyPatch("OnTriggerEnter")]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> VehicleCollisionTrigger_Trans_OnTriggerEnter(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> codes = instructions.ToList();
+
+        MethodInfo log = AccessTools.Method(typeof(Debug), nameof(Debug.Log), [typeof(object)]);
+        for (int i = 0; i < codes.Count; i++)
+        {
+            if (codes[i].opcode == OpCodes.Ldstr && (string)codes[i].operand == "Truck collision: {0} || {1} || {2}")
+            {
+                for (int j = i; j < codes.Count; j++)
+                {
+                    if (codes[j].opcode == OpCodes.Call && codes[j].operand as MethodInfo == log)
+                    {
+                        codes[j].opcode = OpCodes.Nop;
+                        break;
+                    }
+
+                    codes[j].opcode = OpCodes.Nop;
+                }
+                CruiserImproved.LogDebug($"Transpiler (Cruiser collision): Resolve NRE by removing unnecessary log");
+                return codes;
+            }
+        }
+
+        CruiserImproved.LogWarning($"Cruiser collision transpiler failed");
+        return codes;
+    }
+
     [HarmonyPatch("OnTriggerEnter")]
     [HarmonyPrefix]
     static bool OnTriggerEnter_Prefix(VehicleCollisionTrigger __instance, Collider other)
