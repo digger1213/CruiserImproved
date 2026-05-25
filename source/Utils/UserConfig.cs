eb247f38c80f9d9694304ac22d65ad42abcaf72b
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2026-05-25 04:24:22 +0100

    Update UserConfig.cs

--- source/Utils/UserConfig.cs
+++ source/Utils/UserConfig.cs
@@ -71,7 +71,7 @@ internal class UserConfig
             "\nCan display information according to values set. If multiple settings are specified for the same information, the most detailed is used.");
         CabinLightToggle = config.Bind("General", "Cabin Light Toggle", true, "If true, allow toggling the cabin light by interacting with it.\nThe light state will be synced with other CruiserImproved users.");
 
-        AcceptableValueRange<float> seatScale = new(0f, 0.75f);
+        AcceptableValueRange<float> seatScale = new(0f, 1f);
         SeatBoostScale = config.Bind("General", "Seat Boost Scale", 0.5f, new ConfigDescription("How much to boost the seat up? Set 0 to disable.", seatScale));
 
         //Cruiser Health
