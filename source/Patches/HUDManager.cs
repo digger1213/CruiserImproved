6855e36318900c445f6da47279343251409123f6
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2026-05-30 14:19:08 +0100

    Update HUDManager.cs

--- source/Patches/HUDManager.cs
+++ source/Patches/HUDManager.cs
@@ -13,7 +13,9 @@ internal class HUDManagerPatches
         if (!NetworkSync.Config.ScanWhileSeated || __result) return;
 
         //override to allow scan while seated
-        if (GameNetworkManager.Instance.localPlayerController.inVehicleAnimation && !GameNetworkManager.Instance.localPlayerController.isPlayerDead)
+        PlayerControllerB localPlayer = GameNetworkManager.Instance.localPlayerController;
+        bool validCruiser = localPlayer.inVehicleAnimation && localPlayer.currentTriggerInAnimationWith && localPlayer.currentTriggerInAnimationWith.overridePlayerParent;
+        if (validCruiser && !localPlayer.isPlayerDead && localPlayer.currentTriggerInAnimationWith.overridePlayerParent.TryGetComponent<VehicleController>(out var controller) && controller.vehicleID == 0)
             __result = true;
     }
 }
